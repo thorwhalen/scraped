47de60118c121198fd55763ba4dec01c8f407cf1
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-10-27 08:18:08 +0100

    chore: html_to_markdown in root

--- scraped/__init__.py
+++ scraped/__init__.py
@@ -1,6 +1,6 @@
 """Tools for scraping"""
 
-from scraped.util import download_site, markdown_of_site
+from scraped.util import download_site, markdown_of_site, html_to_markdown
 from scraped.tools import scrape_multiple_sites
 
 # ------------------------------------------------------------------------------
